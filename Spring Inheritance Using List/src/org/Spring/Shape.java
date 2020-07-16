a82c26270d95f1296e3dd167f190ab8fdc4cffe7
ankit <ankit@DESKTOP-A4EU1SK>
2020-07-16 17:45:02 +0530

    Inheeritance using list

---
+++ Spring Inheritance Using List/src/org/Spring/Shape.java
@@ -0,0 +1,16 @@
+package org.Spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.SpringVersion;
+
+@SuppressWarnings("deprecation")
+public class Shape {
+
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle=(Triangle) context.getBean("triangle");
+		triangle.draw();
+	}
+
+}
---
+++ Spring Inheritance Using List/src/org/Spring/Triangle.java
@@ -0,0 +1,28 @@
+package org.Spring;
+
+import java.util.List;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle {
+	private List<Point> points;
+
+
+	public List<Point> getPoints() {
+		return points;
+	}
+
+
+	public void setPoints(List<Point> points) {
+		this.points = points;
+	}
+
+	public void draw() {
+		for (Point point : points) {
+			System.out.println(point);
+		}
+	}
+}
